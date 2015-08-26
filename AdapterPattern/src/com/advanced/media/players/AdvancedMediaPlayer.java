606773c1110a749767ee51d1fe12d47e3251e334
dan.testehan <dan.testehan@accenture.com>
2015-08-26 15:03:46 +0300

    added a comment

--- AdapterPattern/src/com/advanced/media/players/AdvancedMediaPlayer.java
+++ AdapterPattern/src/com/advanced/media/players/AdvancedMediaPlayer.java
@@ -3,5 +3,5 @@ package com.advanced.media.players;
 public interface AdvancedMediaPlayer {
 	   public void playVlc(String fileName);
 	   public void playMp4(String fileName);
-	   
+	   // this is a comment because of reasons
 }
